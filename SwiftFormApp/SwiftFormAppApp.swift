77336027b2ad9e04768682e8d05d42847e9a3845
OlegPlugaru <olegplugaru.md@gmail.com>
2024-02-24 14:25:29 +0200

    Initial Commit

---
+++ SwiftFormApp/SwiftFormAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFormAppApp.swift
+//  SwiftFormApp
+//
+//  Created by Oleg Plugaru on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFormAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
